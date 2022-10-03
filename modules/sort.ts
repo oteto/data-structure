8ded9d83f403b97e1a54037d9fc2356ad98fe63a
oteto <58469602+oteto@users.noreply.github.com>
2022-10-03 13:01:35 +0900

    feat: done merge sort and quick sort (#4)

--- modules/sort.ts
+++ modules/sort.ts
@@ -41,6 +41,70 @@ function insertionSort(nums: readonly number[]): number[] {
   return _nums;
 }
 
+function mergeSort(nums: readonly number[]): number[] {
+  const _nums = [...nums];
+  if (_nums.length < 2) {
+    return _nums;
+  }
+  const mid = Math.floor(_nums.length / 2);
+  return marge(
+    mergeSort(_nums.slice(0, mid)),
+    mergeSort(_nums.slice(mid)),
+  );
+}
+
+function marge(nums1: readonly number[], nums2: readonly number[]): number[] {
+  let [i1, i2] = [0, 0];
+  const nums: number[] = [];
+  while (i1 < nums1.length && i2 < nums2.length) {
+    if (nums1[i1] <= nums2[i2]) {
+      nums.push(nums1[i1]);
+      i1++;
+    } else {
+      nums.push(nums2[i2]);
+      i2++;
+    }
+  }
+  while (i1 < nums1.length) {
+    nums.push(nums1[i1]);
+    i1++;
+  }
+  while (i2 < nums2.length) {
+    nums.push(nums2[i2]);
+    i2++;
+  }
+  return nums;
+}
+
+function quickSort(
+  nums: number[],
+  left = 0,
+  right = nums.length - 1,
+): number[] {
+  if (left < right) {
+    const pivotIndex = pivot(nums, left, right);
+    quickSort(nums, left, pivotIndex - 1);
+    quickSort(nums, pivotIndex + 1, right);
+  }
+  return nums;
+}
+
+function pivot(
+  nums: number[],
+  pivotIndex: number,
+  lastIndex: number,
+): number {
+  let swapIndex = pivotIndex;
+  for (let i = swapIndex + 1; i <= lastIndex; i++) {
+    if (nums[i] < nums[pivotIndex]) {
+      swapIndex++;
+      [nums[swapIndex], nums[i]] = [nums[i], nums[swapIndex]];
+    }
+  }
+  [nums[swapIndex], nums[pivotIndex]] = [nums[pivotIndex], nums[swapIndex]];
+  return swapIndex;
+}
+
 Deno.test("bubble sort", (_) => {
   assertEquals(
     bubbleSort([4, 7, 5, 1, 10]),
@@ -50,6 +114,8 @@ Deno.test("bubble sort", (_) => {
     bubbleSort([5, 5, 5, 1, 3, 3, 6, 10]),
     [1, 3, 3, 5, 5, 5, 6, 10],
   );
+  assertEquals(mergeSort([3]), [3]);
+  assertEquals(mergeSort([]), []);
 });
 
 Deno.test("selection sort", (_) => {
@@ -57,6 +123,8 @@ Deno.test("selection sort", (_) => {
     selectionSort([10, 4, 4, 2, 7, 6, 1]),
     [1, 2, 4, 4, 6, 7, 10],
   );
+  assertEquals(mergeSort([3]), [3]);
+  assertEquals(mergeSort([]), []);
 });
 
 Deno.test("insertion sort", (_) => {
@@ -64,4 +132,24 @@ Deno.test("insertion sort", (_) => {
     insertionSort([5, 8, 1, 3, 10, 2, 7, 7]),
     [1, 2, 3, 5, 7, 7, 8, 10],
   );
+  assertEquals(mergeSort([3]), [3]);
+  assertEquals(mergeSort([]), []);
+});
+
+Deno.test("merge sort", (_) => {
+  assertEquals(
+    mergeSort([5, 8, 1, 3, 10, 2, 7, 7]),
+    [1, 2, 3, 5, 7, 7, 8, 10],
+  );
+  assertEquals(mergeSort([3]), [3]);
+  assertEquals(mergeSort([]), []);
+});
+
+Deno.test("quick sort", (_) => {
+  assertEquals(
+    quickSort([5, 8, 1, 3, 10, 2, 7, 7]),
+    [1, 2, 3, 5, 7, 7, 8, 10],
+  );
+  assertEquals(quickSort([3]), [3]);
+  assertEquals(quickSort([]), []);
 });
